68b1f473241591bf873f8996fc00b22df7fdbe8c
ziedlahbib <lahbibmedzied@gmail.com>
2022-06-15 20:55:49 +0100

    Update user.model.ts

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -1,2 +1,16 @@
+import { RowOutlet } from "@angular/cdk/table";
+import { Commande } from "./commande.model";
+
 export class User {
+    id: Number;
+    userName: String;
+    email: String;
+    password: String;
+    name: String;
+    lastName: String;
+    address: String;
+    active: Boolean;
+    roles:RowOutlet;
+    cmd:Commande;
+ 
 }
